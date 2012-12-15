664dc124f5c4487f77964df08c5639272b99dfbb
dmitrymov <dimmov21@gmail.com>
2012-12-15 17:04:39 +0200

    Added unit trst

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -4,18 +4,31 @@ public class RomanNumerals {
 	public String toRoman(int number){
 		if(number <= 0 || number >= 4000)
 			return "";
-		String ans = "";
-		while(number > 9) {
-			ans = ans + "X";
+		switch(number) {
+		case(1):
+			return "I";
+		case(2):
+			return "II";
+		case(3):
+			return "III";
+		case(4):
+			return "IV";
+		case(5):
+			return "V";
+		case(6):
+			return "VI";
+		case(7):
+			return "VII";
+		case(8):
+			return "VIII";
+		case(9):
+			return "IX";
+		case(10):
+			return "X";		
 		}
-		while(number > 4) {
-			ans = ans + "V";
-		}
-		while(number > 0) {
-			ans = ans + "I";
-		}
-		int num = 0;
+		int i = 0;
 		return null;
+		
 	}
 	
 }
