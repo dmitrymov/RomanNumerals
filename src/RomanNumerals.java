c1cc0ff1bb5a5148bde969f0d68379041ed534ce
dmitrymov <dimmov21@gmail.com>
2012-12-15 17:08:34 +0200

    changed test condition

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -4,29 +4,9 @@ public class RomanNumerals {
 	public String toRoman(int number){
 		if(number <= 0 || number >= 4000)
 			return "";
-		switch(number) {
-		case(1):
-			return "I";
-		case(2):
-			return "II";
-		case(3):
-			return "III";
-		case(4):
-			return "IV";
-		case(5):
-			return "V";
-		case(6):
-			return "VI";
-		case(7):
-			return "VII";
-		case(8):
-			return "VIII";
-		case(9):
-			return "IX";
-		case(10):
-			return "X";		
-		}
-		int i = 0;
+		String ans = "";
+		while(number > 9)
+			ans = ans + "X";
 		return null;
 		
 	}
